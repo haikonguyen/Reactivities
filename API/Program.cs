c58a1e34cc3f8ca8114de0b9bfe7ad374b681213
Hai Ha Nguyen <haicorp87@gmai.com>
2020-09-09 23:59:55 +0200

    upgrading Nugets, fixing ILogger issue

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,7 +8,6 @@ using Persistence;
 
 namespace API
 {
-    //testing commit
     public class Program
     {
         public static void Main(string[] args)
@@ -30,7 +25,7 @@ namespace API
                 }
                 catch (Exception ex)
                 {
-                    var logger = services.GetRequiredService<ILogger>();
+                    var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
                 }
             }
