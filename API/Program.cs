32e4f14ea36d0a6721460b1e0fae1a0633f5ab21
Hai Ha Nguyen <haicorp87@gmai.com>
2020-09-13 13:15:51 +0200

    fixing BE - getting files from Backup

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -25,7 +29,7 @@ namespace API
                 }
                 catch (Exception ex)
                 {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    var logger = services.GetRequiredService<ILogger>();
                     logger.LogError(ex, "An error occured during migration");
                 }
             }
